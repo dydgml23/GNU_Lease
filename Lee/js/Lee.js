8107a02f1644a21a1cf034a778c29321eda74eca
dydgml23 <dydgml23@naver.com>
2021-09-27 17:55:47 +0900

    0927

--- Lee/js/Lee.js
+++ Lee/js/Lee.js
@@ -2,4 +2,7 @@ $(function(){
   $('#topul li').click(function(){
     $(this).addClass('on').siblings().removeClass('on')
   });
+  $('#mdul li').click(function(){
+    $(this).addClass('on').siblings().removeClass('on')
+  });
 });
