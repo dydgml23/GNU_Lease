05e0d49cbd30d7a67330903725715f597c61aaa7
dydgml23 <dydgml23@naver.com>
2021-09-30 17:59:18 +0900

    0930 work

--- Lee/js/Lee.js
+++ Lee/js/Lee.js
@@ -1,8 +1,18 @@
 $(function(){
-  $('#topul li').click(function(){
-    $(this).addClass('on').siblings().removeClass('on')
+  $('#topul li .cmd a').click(function(){
+    $('#topcmd').addClass('on');
+    $('#topfree').removeClass('on');
   });
-  $('#mdul li').click(function(){
-    $(this).addClass('on').siblings().removeClass('on')
+  $('#topul li .freeleas a').click(function(){
+    $('#topfree').addClass('on');
+    $('#topcmd').removeClass('on');
+  });
+  $('#mdul li .new a').click(function(){
+    $('#mdnew').addClass('on');
+    $('#mdhot').removeClass('on');
+  });
+  $('#mdul li .hot a').click(function(){
+    $('#mdhot').addClass('on');
+    $('#mdnew').removeClass('on');
   });
 });
